404c2947c402f5bd4904f0af26f1b27306cac5c9
Jillian Breau <jmbreau@mun.ca>
2022-02-27 17:20:31 -0330

    feat: Set up hero banner layout

---
+++ components/layout.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import styles from './layout.module.css';
+import Link from 'next/link';
+
+export default function Layout({
+  home,
+  children,
+}: {
+  home?: boolean,
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Fetch and display a list of projects"
+        />
+      </Head>
+      <header>
+        <div className={styles.hero}>
+          {!home && <h1 className={styles.back}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </h1>}
+          <h1 className={styles.title}>
+            {`Display project${home ? 's' : ' info'}`}
+          </h1>
+        </div>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+};
---
+++ pages/[project].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const ProjectPage: NextPage = () => {
+  return (
+    <Layout>
+    </Layout>
+  )
+}
+
+export default ProjectPage;
